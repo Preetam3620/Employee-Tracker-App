96ab0e3722fc2a10a08e934361a161ee3baf6cb8
DigvijayIngole55 <64873101+DigvijayIngole55@users.noreply.github.com>
2020-09-24 16:37:05 +0530

    Login Page update 1.2

--- emp_tracker/lib/screens/loginScreen.dart
+++ emp_tracker/lib/screens/loginScreen.dart
@@ -7,172 +7,213 @@ class LoginScreen extends StatefulWidget {
   _LoginScreenState createState() => _LoginScreenState();
 }
 
+class Item {
+  const Item(this.name,);
+  final String name;
+}
 class _LoginScreenState extends State<LoginScreen> {
   bool _isRemember = false;
+  Item selectedUser;
+  List<Item> users = <Item>[
+    const Item('Employee',),
+    const Item('Interveiwee',),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 3,
-              child: Container(),
-            ),
-            Expanded(
-              flex: 5,
-              child: Container(
-                child: Image.asset(
-                  'images/logo1.png',
-                  width: 200.0,
+        body: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                flex: 3,
+                child: Container(),
+              ),
+              Expanded(
+                flex: 5,
+                child: Container(
+                  child: Image.asset(
+                    'images/logo1.png',
+                    width: 200.0,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 60.0,
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                padding: EdgeInsets.only(left: 35.0, right: 35.0),
-                child: TextField(
-                  style: TextStyle(color: Colors.black),
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
+              SizedBox(
+                height: 60.0,
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  padding: EdgeInsets.only(left: 35.0, right: 35.0),
+                  child: TextField(
+                    style: TextStyle(color: Colors.black),
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
 
-                    fillColor: Colors.white,
-                    filled: true,
-                    prefixIcon: Icon(
-                      Icons.account_circle,
-                      color: Colors.black,
-                    ),
-                    hintText: ('Enter your Email'),
-                    hintStyle: TextStyle(
-                      color: Colors.black,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
+                      fillColor: Colors.white,
+                      filled: true,
+                      prefixIcon: Icon(
+                        Icons.account_circle,
+                        color: Colors.black,
+                      ),
+                      hintText: ('Enter your Email'),
+                      hintStyle: TextStyle(
+                        color: Colors.black,
+                      ),
+                      enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(15.0),
-                        borderSide:
-                            BorderSide(color: Color(0xFF64DD17), width: 2.0)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide:
+                              BorderSide(color: Color(0xFF64DD17), width: 2.0)),
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                padding: EdgeInsets.only(left: 35.0, right: 35.0),
-                child: TextField(
-                  style: TextStyle(color: Colors.black),
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Colors.black,
-                    ),
-                    hintText: ('Enter your password'),
-                    hintStyle: TextStyle(
-                      color: Colors.black,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  padding: EdgeInsets.only(left: 35.0, right: 35.0),
+                  child: TextField(
+                    style: TextStyle(color: Colors.black),
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.black,
+                      ),
+                      hintText: ('Enter your password'),
+                      hintStyle: TextStyle(
+                        color: Colors.black,
+                      ),
+                      enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(15.0),
-                        borderSide:
-                            BorderSide(color: Color(0xFF00C2CB), width: 2.0)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide:
+                              BorderSide(color: Color(0xFF00C2CB), width: 2.0)),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.only(left: 35.0, right: 35.0),
-                child: Row(
-                  children: <Widget>[
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Container(
-                      width:20.0,
-                      height: 20.0,
-                      color: Colors.grey,
-                      child: Checkbox(
-                        value: _isRemember,
-                        onChanged: (bool value) {
+              Expanded(
+                flex: 1,
+                child: Container(
+                  padding: EdgeInsets.only(left: 35.0, right: 35.0),
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Container(
+                        width:20.0,
+                        height: 20.0,
+                        color: Colors.grey,
+                        child: Checkbox(
+                          value: _isRemember,
+                          onChanged: (bool value) {
+                            setState(() {
+                              _isRemember = value;
+                            });
+                          },
+                          activeColor: Colors.black,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 15.0,
+                      ),
+                      Text(
+                        'Remember Me',
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 30.0,
+                      ),
+                      DropdownButton<Item>(
+                        hint:  Text("Select item"),
+                        value: selectedUser,
+                        onChanged: (Item Value) {
                           setState(() {
-                            _isRemember = value;
+                            selectedUser = Value;
                           });
                         },
-                        activeColor: Colors.black,
+                        items: users.map((Item user) {
+                          return  DropdownMenuItem<Item>(
+                            value: user,
+                            child: Row(
+                              children: <Widget>[
+                                SizedBox(width: 10,),
+                                Text(
+                                  user.name,
+                                  style:  TextStyle(color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          );
+                        }).toList(),
+                        dropdownColor: Colors.white,
                       ),
-                    ),
-                    SizedBox(
-                      width: 15.0,
-                    ),
-                    Text(
-                      'Remember Me',
-                      style: TextStyle(
-                        color: Colors.black54,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Expanded(
-              flex: 2,
-              child:  GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (context) => Home())
-                  );
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.only(left: 70.0,right: 70.0),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Color(0xFF64DD17),
-                        Color(0xFF00C2CB),
-                      ],
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                    ),
-                    color: Colors.black,
-                      borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  child: Text("Login",
-                  style: TextStyle(
-                      color: Colors.black,
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.w600,
-                  ),),
-            ),
+              SizedBox(
+                height: 30.0,
               ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Container(),
-            ),
-          ],
+              Expanded(
+                flex: 2,
+                child:  GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => Home())
+                    );
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(left: 70.0,right: 70.0),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0xFF64DD17),
+                          Color(0xFF00C2CB),
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      color: Colors.black,
+                        borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    child: Text("Login",
+                    style: TextStyle(
+                        color: Colors.black,
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.w600,
+                    ),),
+              ),
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Container(),
+              ),
+            ],
+          ),
+          ),
         ),
       ),
     );
