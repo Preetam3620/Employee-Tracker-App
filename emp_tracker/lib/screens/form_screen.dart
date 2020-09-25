591f23431355f2d73b9a7ef85db79448df52e815
Preetam Maske <preetamamaske1@gmail.com>
2020-09-25 16:42:26 +0530

    feedback form alert added

--- emp_tracker/lib/screens/form_screen.dart
+++ emp_tracker/lib/screens/form_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'loginScreen.dart';
+
 class FormScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -45,7 +48,7 @@ class FormScreenState extends State<FormScreen> {
         }
 
         if (!RegExp(
-            r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
             .hasMatch(value)) {
           return 'Please enter a valid email Address';
         }
@@ -56,7 +59,7 @@ class FormScreenState extends State<FormScreen> {
         _email = value;
       },
     );
-}
+  }
 
   Widget _buildAnswer1() {
     return TextFormField(
@@ -126,81 +129,105 @@ class FormScreenState extends State<FormScreen> {
     );
   }
 
+  bool sumbitPressed = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(title: Text("Interview Feedback"),
-          backgroundColor: Color(0xFF00C2CB),),
+      appBar: AppBar(
+        title: Text("Interview Feedback"),
+        backgroundColor: Color(0xFF00C2CB),
+      ),
       body: Container(
         margin: EdgeInsets.only(left: 10, right: 10),
         child: Form(
           key: _formKey,
-          
           child: SingleChildScrollView(
             child: SizedBox(
               height: MediaQuery.of(context).size.height,
-
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text('Enter your Full Name',
+                  Text(
+                    'Enter your Full Name',
                     style: TextStyle(
                       fontSize: 18.0,
-
-                    ) ,),
+                    ),
+                  ),
                   _buildName(),
-                  Text('Enter your Email-id',
+                  Text(
+                    'Enter your Email-id',
                     style: TextStyle(
                       fontSize: 18.0,
-
-                    ) ,),
+                    ),
+                  ),
                   _buildEmail(),
-                  SizedBox(height: 10.0,),
-                  Text('Rate your Interview Experience',
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    'Rate your Interview Experience',
                     style: TextStyle(
                       fontSize: 18.0,
-                    ) ,),
-
+                    ),
+                  ),
                   _buildAnswer1(),
-                  Text('Were you able to get your points across?',
+                  Text(
+                    'Were you able to get your points across?',
                     style: TextStyle(
                       fontSize: 18.0,
-
-                    ) ,),
+                    ),
+                  ),
                   _buildAnswer2(),
-                  Text('What could you have done better?',
+                  Text(
+                    'What could you have done better?',
                     style: TextStyle(
                       fontSize: 18.0,
-                    ) ,),
+                    ),
+                  ),
                   _buildAnswer3(),
-                  Text('Any personal feedback regarding your interview?',
+                  Text(
+                    'Any personal feedback regarding your interview?',
                     style: TextStyle(
                       fontSize: 18.0,
-                    ) ,),
+                    ),
+                  ),
                   _buildAnswer4(),
-
                   RaisedButton(
                     child: Text(
                       'Submit',
-                      style: TextStyle(color: Color(0xFF00C2CB) , fontSize: 14),
+                      style: TextStyle(color: Color(0xFF00C2CB), fontSize: 14),
                     ),
                     onPressed: () {
+                      setState(() {
+                        sumbitPressed = true;
+                      });
                       if (!_formKey.currentState.validate()) {
                         return;
                       }
-
                       _formKey.currentState.save();
-
-                      print(_name);
-                      print(_email);
-                      print(_Ans1);
-                      print(_Ans2);
-                      print(_Ans3);
-                      print(_Ans4);
-                      _Firestore.collection('feedback').add({'name':_name,'emailid':_email,'answer1':_Ans1,'answer2':_Ans2,'answer3':_Ans3,'answer4':_Ans4});
-
+                      _Firestore.collection('feedback').add({
+                        'name': _name,
+                        'emailid': _email,
+                        'answer1': _Ans1,
+                        'answer2': _Ans2,
+                        'answer3': _Ans3,
+                        'answer4': _Ans4
+                      });
+                      Alert(
+                          context: context,
+                          type: AlertType.success,
+                          title: 'Response Submitted!',
+                          desc: 'Thank you for your response',
+                          style: AlertStyle(
+                            backgroundColor: Colors.white,
+                          )).show();
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoginScreen()));
                       //Send to API
                     },
                   )
